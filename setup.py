02ff71b1272242aa5d32c3005ec1411a78be8511
Juttaro <juttaro@gmail.com>
2024-09-27 15:44:47 -0400

    update setup.py and setup.cfg to handle install

--- setup.py
+++ setup.py
@@ -1,37 +1,18 @@
-"""This module contains the packaging routine for the pybook package"""
-
 from setuptools import setup, find_packages
-import pip
-pip_major_version = int(pip.__version__.split(".")[0])
-if pip_major_version >= 20:
-    from pip._internal.req import parse_requirements
-    from pip._internal.network.session import PipSession
-elif pip_major_version >= 10:
-    from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-else:
-    from pip.req import parse_requirements
-    from pip.download import PipSession
 
-
-def get_requirements(source):
-    """Get the requirements from the given ``source``
-
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
+with open('requirements/requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
-    packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
+    name='scrapy-selenium',
+    version='1.2.1',
+    author='James Uttaro',
+    author_email='james@classic.com',
+    url='https://github.com/straatdotco/scrapy-selenium',
+    license='MIT',
+    description='Scrapy with selenium',
+    packages=find_packages(exclude=['*tests*']),
+    install_requires=requirements
 )
 
 
